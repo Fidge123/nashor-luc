7d6ab9404ce9329616304a9b69e188c9d93db4a1
Florian Richter <florian.richter@sap.com>
2017-04-23 16:44:07 +0200

    Change to use dist for pages setup

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  ng build --aot --prod
+  ng build --aot --prod --base-href="/nashor-luc/" --progress="false"
 }
 
 # Save some useful information
@@ -13,22 +13,14 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
-
-# Clean out existing contents
-rm -rf out/* || exit 0
-
 # Run our compile script
 doCompile
-cp dist/* out/
 
 # Now let's go have some fun with the cloned repo
-cd out
+cd dist
+git init
+git remote add origin SSH_REPO
+
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
@@ -54,4 +46,4 @@ eval `ssh-agent -s`
 ssh-add deploy_key
 
 # Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+git push $SSH_REPO HEAD:$TARGET_BRANCH --force
